5c5a51587b57214b586f70e7a6a5aa4673a76d85
Nlipski <13ml76@queensu.ca>
2019-02-01 23:52:39 -0500

    App response

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
+from flask import Flask, request, session
+import googlemaps
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+gmaps = googlemaps.Client(key='AIzaSyBUlQyHBJsv-GBooA_64cyA_9q-abYSehE')
 
 account = "AC07e3abdcc5ca8ab68816022080fb9331"
 token = "e50d782ec9b8c284db8ab0f537ed0fbc"
 client = Client(account, token)
 
-from_number = "+19027108014"
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+
+    resp_body="Sucks to suck"
+
+    body = request.values.get('Body', None)
+    resp = MessagingResponse()
+
+    now = datetime.now()
+    directions_result = gmaps.directions("Sydney Town Hall",
+                                         "Parramatta, NSW",
+                                         mode="transit",
+                                         departure_time=now)
+    print (directions_result)
+    if body != "I'm lost":
+        resp_body = "Hmmm didn't get it"
+
+    resp.message(resp_body)
+
+    return str(resp)
 
 
-message = client.messages.create(to="+16476096539", from_=from_number,
-                                 body="")
+if __name__ == "__main__":
+    app.run(debug=True)
+print(message.sid)
