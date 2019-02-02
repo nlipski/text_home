016fea938f6b161751f78a6f40ead4641811bb44
Jonathon Malcolm <j_malcolm@outlook.com>
2019-02-02 01:21:11 -0500

    Better parsing of input

--- GoogleDirections.py
+++ GoogleDirections.py
@@ -1,27 +1,39 @@
 import requests
 import json
 
-incoming_text="I am in Kingston and want to go to Disneyland"
+incoming_text="I am in Kingston and want to walk to Disneyland"
 
 api_key="AIzaSyBUlQyHBJsv-GBooA_64cyA_9q-abYSehE"
 dataa={"document":{"type":"PLAIN_TEXT","content":incoming_text},"encodingType":"UTF16"}
-
 r=requests.post("https://language.googleapis.com/v1beta2/documents:analyzeEntities?key=" + api_key, json=dataa)
 testing = json.loads(r.text)
 testbla = testing["entities"]
-loc1 = ""
-loc2 = ""
+toLoc = ""
+fromLoc = ""
+mode = "driving"
+#print(r.text)
 for entities in testbla:
     if entities["type"] == "LOCATION":
-        if loc1 == "":
-            loc1 = entities["name"]
-        elif loc2 == "":
-            loc2 = entities["name"]
+        index = int(entities["mentions"][0]["text"]["beginOffset"])
+        reference = incoming_text[index-3:index]
+        if reference == "to ":
+            toLoc = entities["name"]
+        else:
+            fromLoc = entities["name"]
+    elif entities["type"] == "OTHER":
+        if entities["name"] == "driving" or entities["name"] == "drive":
+            mode = "driving"
+        if entities["name"] == "walking" or entities["name"] == "walk":
+            mode = "walking"
+        if entities["name"] == "bicycling" or entities["name"] == "bike":
+            mode = "bicycling"
+        if entities["name"] == "transit":
+            mode = "transit"
 
 
-r=requests.get("https://maps.googleapis.com/maps/api/directions/json?origin=" + loc1 + "&destination=" + loc2 + "&key=" + api_key)
+
+r=requests.get("https://maps.googleapis.com/maps/api/directions/json?origin=" + fromLoc + "&destination=" + toLoc + "&mode=" + mode + "&key=" + api_key)
 testing = json.loads(r.text)
-
 something = testing["routes"]
 some = something[0]
 steps = some["legs"][0]["steps"]
