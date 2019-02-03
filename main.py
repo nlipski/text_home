bce8fdc4d8f9dab59b36da84b3a0ef062bd912c1
Connor Sparling <connor.sparling@gmail.com>
2019-02-03 01:13:53 -0500

    changes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import googlemaps
 from inner_functions import get_locations, check_location, parse_directions, locationsClass,parse_image
 from default_classes import storedLocationClass, defaultCustomLocations
 from state_functions import setLocation, setCustomLocationName, setCustomLocationLocation, confirmCustomLocation, getLocations, removeLocations, getTo, setGetTo, confirmTo, getFrom, setGetFrom, confirmFrom, clearConversationState, getHelp, sendLocationHelp, sendThanks, checkConfirm
+from tokens import client, GOOGLE_API_KEY, SECRET_KEY
 import datetime
 import json
 import os
